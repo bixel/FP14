18f5645179f078a3e362848d120cef9645803eaa
Kevin H <kevin@kehei.de>
2014-09-04 15:30:45 +0200

    some files

---
+++ V01_Myonen/data/plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def unbinned_array(binned_array, start=0, end=None, calibration_func=lambda x: x):
+    """ Returned an array with 'raw' Data for creating histograms with plt.hist
+    """
+    raw_data = []
+    if end == None:
+        end = len(binned_array) - 1
+    for x in range(start,end):
+        raw_data.extend([calibration_func(x)] * binned_array[x])
+    return raw_data
+
+data = np.genfromtxt('data.txt', unpack=True)
+unbinned_data = unbinned_array(data)
+plt.hist(unbinned_data, bins=200)
+plt.show()
