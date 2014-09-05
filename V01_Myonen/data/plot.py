b848801bcd207b8fe8d0a31f20b3410aa7972479
kevin.heinicke <kevin.heinicke@tu-dortmund.de>
2014-09-05 12:51:14 +0200

    add ipy-notebook for myons. scaling of histograms is hard to do

--- V01_Myonen/data/plot.py
+++ V01_Myonen/data/plot.py
@@ -1,20 +1,32 @@
-#! /usr/bin/evn python3.4
+#! /usr/bin/env python3.4
 # -*- coding: utf-8 -*-
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 def unbinned_array(binned_array, start=0, end=None, calibration_func=lambda x: x):
     """ Returned an array with 'raw' Data for creating histograms with plt.hist
     """
     raw_data = []
     if end == None:
-        end = len(binned_array) - 1
+        end = len(binned_array)
     for x in range(start,end):
         raw_data.extend([calibration_func(x)] * binned_array[x])
-    return raw_data
+    return np.array(raw_data)
 
 data = np.genfromtxt('myons.txt', unpack=True)
 unbinned_data = unbinned_array(data)
-plt.hist(unbinned_data, bins=200)
+bins = 50
+y, binEdges = np.histogram(unbinned_data, bins=bins)
+bincenters = 0.5 * (binEdges[1:] + binEdges[:-1])
+width = 0.5 * (bincenters[-1] - bincenters[0]) / bins
+medStd = np.sqrt(y)
+plt.errorbar(bincenters, y, fmt='ro', yerr=medStd, xerr=width)
+
+x_data = np.arange(0, len(data))
+decay_function = lambda x, a, b: a * np.exp(b * x)
+coeff, var = curve_fit(decay_function, x_data, data, p0=[1, -1e-2])
+plt.plot(x_data, decay_function(x_data, coeff[0], coeff[1]))
+plt.xlim(0,400)
 plt.show()
