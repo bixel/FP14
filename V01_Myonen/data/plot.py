d8a54e8e4f81e327d3abeb23cc9a33f19ff86d20
Kevin H <kevin@kehei.de>
2014-09-22 16:30:31 +0200

    Some pep8 stuff

--- V01_Myonen/data/plot.py
+++ V01_Myonen/data/plot.py
@@ -5,13 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def unbinned_array(binned_array, start=0, end=None, calibration_func=lambda x: x):
+
+def unbinned_array(binned_array, start=0, end=None,
+                   calibration_func=lambda x: x):
     """ Returned an array with 'raw' Data for creating histograms with plt.hist
     """
     raw_data = []
-    if end == None:
+    if end is None:
         end = len(binned_array)
-    for x in range(start,end):
+    for x in range(start, end):
         raw_data.extend([calibration_func(x)] * binned_array[x])
     return np.array(raw_data)
 
@@ -28,5 +30,5 @@ x_data = np.arange(0, len(data))
 decay_function = lambda x, a, b: a * np.exp(b * x)
 coeff, var = curve_fit(decay_function, x_data, data, p0=[1, -1e-2])
 plt.plot(x_data, decay_function(x_data, coeff[0], coeff[1]))
-plt.xlim(0,400)
+plt.xlim(0, 400)
 plt.show()
