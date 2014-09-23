407c43d51fc55bc24f56967e7bbff12aae6db2b3
kevin.heinicke <kevin@kehei.de>
2014-09-23 23:12:31 +0200

    too much hackyhack... dont know what im doing

--- V18_Ge-Detektor/data/hist.py
+++ V18_Ge-Detektor/data/hist.py
@@ -1,34 +1,37 @@
-#! /usr/bin/env python3.3
-# -*- coding: utf-8 -*-
-
+#! /usr/bin/python3
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit as cfit
 from scipy.integrate import simps
-from uncertainties import ufloat, umath
+from scipy.stats import sem
+from uncertainties import ufloat, umath, unumpy
 # import peakdetect
 
 
-def sigma_delta(pos, arr, fit_width=30, plot='', calibration_func=None):
+def gauss(x, A, mu, sigma, B):
+    return 0.5 * (np.sign(-B) + 1) \
+        + A * np.exp(- 0.5 * ((x-mu)/sigma)**2)
+
+
+def sigma_delta(pos, arr, fit_width=30, plot='', calibration_func=lambda x: x):
     """ Try to fit a Gauss to the given datapoints and return the integral of
         the function, aswell as the width sigma.
 
         @return the integral (simpson integration) of the gauss function from
                 -fit_width to fit_width
     """
-    gauss = lambda x, A, mu, sigma, B: B + A * np.exp(- 0.5 * ((x-mu)/sigma)**2)
     data_xs = np.arange(-fit_width, fit_width)
     data_ys = arr[pos - fit_width:pos + fit_width]
     init_values = [arr[pos], 0., 5., 0.]
     coeff, var_matrix = cfit(gauss, data_xs, data_ys, p0=init_values)
-    xs = np.linspace(-fit_width, fit_width, 1000)
+    xs = np.linspace(-fit_width, fit_width, 100000)
     if plot != '':
-        plt.plot(xs + pos, gauss(xs, coeff[0], coeff[1], coeff[2], coeff[3]))
+        plt.plot(xs + pos, gauss(xs, *coeff))
         plt.errorbar(data_xs + pos, data_ys, yerr=np.sqrt(data_ys), fmt='.')
         plt.ylim(ymin=-5)
         sigma = coeff[2]
         plt.xlim(pos - 5*sigma, pos + 5*sigma)
-        if calibration_func != None:
+        if calibration_func is None:
             xticks, xlabels = plt.xticks()
             xlabels = list(map(lambda x: '%.1f' % x, calibration_func(xticks)))
             plt.xticks(xticks, xlabels)
@@ -44,7 +47,8 @@ def sigma_delta(pos, arr, fit_width=30, plot='', calibration_func=None):
         var_matrix
     )
 
-def peaks(dist, n_max=10, vetos = []):
+
+def peaks(dist, n_max=10, vetos=[]):
     """ Generate an array with position of maximum value and maximum value
         for given distribution-array.
         Look for n_max maxima.
@@ -62,13 +66,19 @@ def peaks(dist, n_max=10, vetos = []):
         no_max_dist[max_val_index - 10:max_val_index + 10] = 0
     return maxima
 
-def unbinned_array(binned_array, start=0, end=None, calibration_func=lambda x: x):
+
+def unbinned_array(
+    binned_array,
+    start=0,
+    end=None,
+    calibration_func=lambda x: x
+):
     """ Returned an array with 'raw' Data for creating histograms with plt.hist
     """
     raw_data = []
-    if end == None:
+    if end is None:
         end = len(binned_array) - 1
-    for x in range(start,end):
+    for x in range(start, end):
         raw_data.extend([calibration_func(x)] * binned_array[x])
     return raw_data
 
@@ -129,11 +139,21 @@ vetos = np.array(
 )
 maxima = peaks(europium_distribution, vetos=vetos)
 
+errors = np.array([])
+
 ax = plt.axes()
-# plt.plot(x_values, europium_distribution[:len(x_values)])
-# plt.plot(x_values, (eur_time / bkg_time) * background_dist[:len(x_values)])
-heights, positions, bars = plt.hist(unbinned_array(europium_distribution, end=4200), bins=200, histtype='stepfilled', edgecolor='none')
+heights, positions, bars = plt.hist(
+    unbinned_array(europium_distribution, end=4200),
+    bins=200, histtype='stepfilled', edgecolor='none'
+)
 for maximum in maxima:
+    # calculate each error
+    events, coeff, var = sigma_delta(
+        maximum[0], europium_distribution, plot='foo'
+    )
+    errors = np.append(errors, var[1][1])
+
+    # draw arrow for each bar
     height = 0
     for h, pos in zip(heights, positions):
         if pos >= maximum[0] - 4200/200:
@@ -142,6 +162,7 @@ for maximum in maxima:
     ax.arrow(maximum[0], height + 500,
              0, -300,
              head_width=60.0, head_length=100.0, edgecolor='black', facecolor='black')
+
 plt.ylim(ymax=15000)
 plt.title("Detektion der Maxima")
 plt.xlabel("Kanal")
@@ -176,7 +197,8 @@ def calibrated(x):
 calibration_fig = plt.subplot(111)
 x_values = np.arange(0, 4500)
 plt.plot(x_values, calibration_function(x_values, calibration_coeff[0], calibration_coeff[1]))
-plt.errorbar(channels, eu_spectrum[:, 0], 0, 0, "k+")
+plt.errorbar(channels, eu_spectrum[:, 0], xerr=10 * errors, fmt="r+")
+print(errors)
 plt.xlabel("Kanal")
 plt.ylabel("Energie [keV]")
 plt.title("Kalibrationsfit")
@@ -204,17 +226,46 @@ for maximum in maxima:
         )[0]
     )
 
-efficiencies = europium_events / (grand_total * eu_spectrum[:,1])
+efficiencies = europium_events / (grand_total * eu_spectrum[:, 1])
+n_activity = ufloat(activity, np.std(activity))
+n_time = ufloat(eur_time, 1)
+n_grand_total = n_activity * n_time
+n_events = unumpy.uarray(europium_events, np.std(europium_events))
+n_efficiencies = n_events / (n_grand_total * eu_spectrum[:, 1])
+print('grand_total: {}\neff_errors: {}\n'.format(n_grand_total, unumpy.std_devs(n_efficiencies)))
 
-for measured, expected, efficiency in zip(europium_events, grand_total * eu_spectrum[:,1], efficiencies):
-    print('{:.0f}\t{:.0f}\t{:2.1f}'.format(measured, expected, 100 * efficiency))
+# europium_events = unumpy.umatrix(europium_events, np.std(europium_events))
+# eur_time = ufloat(eur_time, 1)
+# activity = ufloat(activity, np.std(activity))
+# grand_total = eur_time * activity
+# efficiencies = europium_events / (grand_total * eu_spectrum[:,1])
 
-# eff_function = lambda x, a, b, c, d: a * np.power(x - b, c) + d
-# eff_function = lambda x, a, b: a / x + b
-# eff_function = lambda x, a, b, c: a * np.exp(-x / b) + c
-eff_function = lambda x, a, b: a * np.exp(-b * x)
-# coeff, var = cfit(eff_function, eu_spectrum[:, 0][[0,5,6,7,8,9]], efficiencies[:][[0,5,6,7,8,9]], p0=[1.0, 0.001], maxfev=10000)
-q_coeff, q_var = cfit(eff_function, eu_spectrum[:, 0], efficiencies, p0=[1.0, 0.001], maxfev=10000)
+for measured, expected, efficiency in zip(
+    europium_events,
+    grand_total * eu_spectrum[:, 1],
+    efficiencies
+):
+    print('{:.0f}\t{:.0f}\t{:2.1f}'.format(
+        measured,
+        expected,
+        100 * efficiency)
+    )
+
+
+def eff_function(x, a, b):  # , c, d):
+    # return a * np.power(x - b, c) + d
+    # return a / x + b
+    # return a * np.exp(-x / b) + c
+    return a * np.exp(-b * x)
+
+q_coeff, q_var = cfit(
+    eff_function,
+    eu_spectrum[:, 0],
+    efficiencies,
+    p0=[1.0, 0.001],
+    maxfev=10000,
+    sigma=unumpy.std_devs(n_efficiencies)
+)
 print(q_coeff, np.sqrt(q_var))
 print(
     'Efficiency\n==========\n'
@@ -227,10 +278,11 @@ print(
     )
 )
 xs = np.arange(50, 1600)
-plt.plot(xs, eff_function(xs, q_coeff[0], q_coeff[1]))# , q_coeff[2], q_coeff[3]))
-plt.plot(eu_spectrum[:, 0], efficiencies, 'b+')
-plt.xlabel('Energie')
+plt.plot(xs, eff_function(xs, *q_coeff))# , q_coeff[2], q_coeff[3]))
+plt.errorbar(eu_spectrum[:, 0], efficiencies, yerr=unumpy.std_devs(n_efficiencies), fmt='r+')
+plt.xlabel('Energie [keV]')
 plt.ylabel('Effizienz')
+plt.ylim(0, 1)
 plt.legend(['Fit Effizienzfunktion', 'Messdaten'], loc='best')
 plt.savefig('05_efficiencies.pdf')
 plt.clf()
@@ -257,25 +309,25 @@ plt.clf()
 
 cal_sig = calibrated(1000 + cs_coeff[2]) - calibrated(1000)
 sigma = ufloat(cal_sig, np.sqrt(cs_var[2][2]) / cs_coeff[2] * cal_sig)
-E12  = 2 * sigma * umath.sqrt(2 * umath.log(2))
+E12 = 2 * sigma * umath.sqrt(2 * umath.log(2))
 E110 = 2 * sigma * umath.sqrt(2 * umath.log(10))
 
-print(
-    'Caesium-Photopeak\n=================\n'
-    'N          = {:g}±{:g}\n'
-    'μ          = {:g}±{:g}keV\n'
-    'σ          = {}\n'
-    'E_1/2      = {}\n'
-    'E_1/10     = {}\n'
-    'E12 / E119 = {}'.format(
-        cs_events, np.sqrt(cs_events),
-        calibrated(cs_maxima[0][0] + cs_coeff[1]), np.sqrt(cs_var[1][1]),
-        sigma,
-        E12,
-        E110,
-        E110 / E12
-    )
-)
+# print(
+#     'Caesium-Photopeak\n=================\n'
+#     'N          = {:g}±{:g}\n'
+#     'μ          = {:g}±{:g}keV\n'
+#     'σ          = {}\n'
+#     'E_1/2      = {}\n'
+#     'E_1/10     = {}\n'
+#     'E12 / E119 = {}'.format(
+#         cs_events, np.sqrt(cs_events),
+#         calibrated(cs_maxima[0][0] + cs_coeff[1]), np.sqrt(cs_var[1][1]),
+#         sigma,
+#         E12,
+#         E110,
+#         E110 / E12
+#     )
+# )
 
 compton_peak = peaks(caesium_dist, n_max=1, vetos=[[0,1300],[1600,5000]])
 reflex_peak = peaks(caesium_dist, n_max=1, vetos=[[1600,5000]])
@@ -367,7 +419,7 @@ for m, e in zip(ba_maxima, ba_events):
 omega = 0.01575
 ba_activity = lambda x, a: omega * a * x
 print(ba_maxima)
-fitx = np.array(eff_function(calibrated(ba_maxima[:,0]), q_coeff[0], q_coeff[1])) * ba_props
+fitx = np.array(eff_function(calibrated(ba_maxima[:,0]), *q_coeff)) * ba_props
 ba_activity_coeff, ba_activity_var = cfit(ba_activity, fitx, ba_events)
 plt.plot(fitx, ba_events, 'rx')
 xs = np.linspace(0, 0.2, 100)
@@ -411,11 +463,11 @@ print('Wooden Coal\n===========')
 for m, e in zip(coal_maxima, coal_events):
     print('E = {:g}\tN = {:g}'.format(calibrated(m[0]), e))
 
-a_bi = 330. / eff_function(609.2, q_coeff[0], q_coeff[1]) \
+a_bi = 330. / eff_function(609.2, *q_coeff) \
      * 1. / 0.47 / coal_time
-a_cs = 1862. / eff_function(661.7, q_coeff[0], q_coeff[1]) \
+a_cs = 1862. / eff_function(661.7, *q_coeff) \
      * 1. / 0.8899 / coal_time
-a_k = 565. / eff_function(1461.6, q_coeff[0], q_coeff[1]) \
+a_k = 565. / eff_function(1461.6, *q_coeff) \
     * 1. / 0.1155 / coal_time
 print('A_coal = {}'.format(a_bi + a_cs + a_k))
 coal_n = ufloat(sum(coal_dist), np.sqrt(sum(coal_dist)))
