b4a64d3d2c17e9d9cc37020785efe06d80e8044d
kevin.heinicke <kevin@kehei.de>
2014-10-03 11:13:03 +0200

    add some comments

--- V18_Ge-Detektor/analysis/caesium.py
+++ V18_Ge-Detektor/analysis/caesium.py
@@ -5,13 +5,14 @@ from unbinner import unbinned_array
 from calibration import E
 
 if __name__ == '__main__':
+    # calibration
     cal_coeff, cal_errs = np.genfromtxt('../build/data/calibration.txt',
             unpack=True)
     cs_dist = np.genfromtxt('../data/Caesium.txt', unpack=True)[:5000]
     xs = E(np.arange(0, 5000), *cal_coeff)
-    print(len(cs_dist), len(xs))
+
+    # maximum-detection and fit of gaussian
     maxima = get_maxima(cs_dist, n_max=1)
-    print(maxima)
     fig, ax = plt.subplots(1, 1)
     mu, sigma, A = fit_gauss(xs, cs_dist, xs[maxima[0, 0]], ax=ax, fit_width=5)
     ax.set_xlabel('Energie / keV')
