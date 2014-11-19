30901882ffc94588b017f66eef0c16ed07dfbef1
Kevin H <kevin@kehei.de>
2014-11-19 10:08:08 +0100

    funktionsdefinition

--- V61_HeNe-Laser/TEM.py
+++ V61_HeNe-Laser/TEM.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
+# TEM-00 Mode
 x_00, I_00 = np.genfromtxt('data/TEM_00.txt', unpack=True)
 
 def gauss(r, off, I_0, omega):
@@ -21,13 +22,19 @@ print('omega: {:g}'.format(omega))
 
 theory_xs = np.linspace(2, 7, 200)
 theory_ys = gauss(theory_xs, *coeff)
-plt.plot(theory_xs, theory_ys)
+plt.plot(theory_xs, theory_ys, 'r-')
 plt.errorbar(x_00, I_00, yerr=0.05*I_00, fmt='b+')
 plt.savefig('build/TEM_00.pdf')
 plt.clf()
 
 
+# TEM-01 Mode
+
+def tem01(r, off, I_0, omega):
+    return I_0 * ((r - off)/omega)**2 * gauss(r, off I_0, omega)
+
 x_01, I_01 = np.genfromtxt('data/TEM_01.txt', unpack=True)
+
 plt.errorbar(x_01, I_01, yerr=0.05*I_01, fmt='b+')
 plt.savefig('build/TEM_01.pdf')
 plt.clf()
