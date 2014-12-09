03dce58fdb0621726efc5e9b51c53dd4a07c7cde
loligsmile <ms_r@hotmail.de>
2014-12-09 11:08:24 +0100

    was passiert gerade?

--- V61_HeNe-Laser/polarisation.py
+++ V61_HeNe-Laser/polarisation.py
@@ -1,6 +1,15 @@
 #! /usr/bin/env python3
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from codecs import open
+from textable import table
+
+
+def sin(x, A, phi):
+    return A * np.sin(x - phi)**2
+
 
 theta, I = np.genfromtxt('data/polarisation.txt', unpack=True)
 
@@ -8,16 +17,45 @@ theta, I = np.genfromtxt('data/polarisation.txt', unpack=True)
 theta = np.append(theta, 360)
 I = np.append(I, I[0])
 
-plt.polar(theta * (np.pi / 180), I, 'bo--')
+coeffs, covar = curve_fit(sin, theta * (np.pi / 180), I,
+                          p0=[0.1, 150*np.pi/180])
+errs = np.sqrt(np.diag(covar))
+I0 = ufloat(coeffs[0], errs[0])
+phi = ufloat(coeffs[1], errs[1])
+xs = np.linspace(0, np.max(theta), 200)
+ys = sin(xs*(np.pi/180), *coeffs)
+
+plt.polar(xs * np.pi / 180, ys, 'r-')
+plt.polar(theta * np.pi / 180, I, 'bo--')
 plt.savefig('build/polarisation_polar.pdf')
 plt.clf()
 
+(open('build/polarisation_I0.tex', 'w', 'utf-8').
+ write(r'$I_0 = \SI{{{:L}}}{{\milli\ampere}}$'.
+ format(I0)))
+(open('build/polarisation_phi.tex', 'w', 'utf-8').
+ write(r'$\varphi = \SI{{{:L}}}{{\degree}}$'.
+ format(phi * 180 / np.pi)))
+
+plt.plot(xs, ys, 'r-', label='Fit')
+
 theta = theta[:-1]
 I = I[:-1]
-plt.errorbar(theta, I, yerr=0.05*I, fmt='bo')
+plt.errorbar(theta, I, yerr=0.05*I, fmt='bo', label='Messpunkte')
 plt.xlabel(r'$\theta/^\circ$')
 plt.ylabel(r'$I/\mathrm{mA}$')
 plt.xlim(-10, 360)
+plt.legend(loc='best')
 plt.savefig('build/polarisation_lin.pdf')
 plt.clf()
 
+f = open('build/polarisation_tab.tex', 'w', 'utf-8')
+f.write(table([r'$\theta/\si{\degree}$', r'$I/\si{\milli\ampere}$',
+               r'$\theta/\si{\degree}$', r'$I/\si{\milli\ampere}$',
+               r'$\theta/\si{\degree}$', r'$I/\si{\milli\ampere}$',
+               r'$\theta/\si{\degree}$', r'$I/\si{\milli\ampere}$'],
+              [theta[:9], I[:9],
+               theta[9:18], I[9:18],
+               theta[18:27], I[18:27],
+               theta[27:36], I[27:36]]))
+f.close()
--- V61_HeNe-Laser/wavelength.py
+++ V61_HeNe-Laser/wavelength.py
@@ -1,33 +1,87 @@
 #! /usr/bin/env python3
 
 import numpy as np
-import uncertainties.unumpy as unp
-from uncertainties import ufloat
 from codecs import open
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from textable import table
 
 # slot
-
-d = 645
-s = 0.05
-offset = 5.8
+d = ufloat(645, 5)
+s = ufloat(0.05, 0.01)
+offset = ufloat(5.8, 0.58)
 
 left, right = np.genfromtxt('data/slot.txt', unpack=True)
 
-left -= offset
-right -= offset
+left -= offset.n
+right -= offset.n
+right = np.round(right, decimals=1)
+
 
 def lam(x, n=1, s=s, d=d):
-    alpha = np.arctan(x/d)
-    return s / n * alpha
+    alpha = unp.arctan(x/d)
+    return s / n * unp.sin(alpha)
 
-lambdas_left = np.array([lam(x, n=n+1) for n, x in enumerate(left)])
-lambdas_right = np.array([lam(x, n=n+1) for n, x in enumerate(right)])
+lambdas_left = unp.uarray([lam(x, n=n+1).n for n, x in enumerate(left)],
+                          [lam(x, n=n+1).s for n, x in enumerate(left)])
+lambdas_right = unp.uarray([lam(x, n=n+1).n for n, x in enumerate(right)],
+                           [lam(x, n=n+1).s for n, x in enumerate(right)])
 lambdas = np.append(np.abs(lambdas_left), np.abs(lambdas_right))
 
 (open('build/lambda_slot.tex', 'w', 'utf-8')
-        .write(r'$\overline{{\lambda}}_\text{{Spalt}}'
-               r'= \SI{{{:3.0f}+-{:3.0f}}}{{\nano\meter}}$'.format(
-                    lambdas.mean() * 1e6, lambdas.std() * 1e6         
-                    )
-              )
-)
+ .write(r'$\overline{{\lambda}}_\text{{Spalt}}'
+        r'= \SI{{{:.0f}+-{:.0f}}}{{\nano\meter}}$'
+        .format(unp.nominal_values(lambdas).mean() * 1e6,
+                unp.std_devs(lambdas).mean() * 1e6)))
+
+f = open('build/lambdas_slot_mm_l.tex', 'w', 'utf-8')
+f.write(table([r'$x/\si{\milli\meter}$', r'$\lambda/\si{\nano\meter}$'],
+              [left, lambdas_left*1e6]))
+f.close()
+f = open('build/lambdas_slot_mm_r.tex', 'w', 'utf-8')
+f.write(table([r'$x/\si{\milli\meter}$', r'$\lambda/\si{\nano\meter}$'],
+              [right, lambdas_right*1e6]))
+f.close()
+
+
+# grid
+s = ufloat(0.01, 0.0)
+d = ufloat(980, 5)
+
+xs1 = 10 * np.genfromtxt('data/grid1.txt', unpack=True)
+xs2_right = np.genfromtxt('data/grid2_right.txt', unpack=True)
+xs2_left = np.genfromtxt('data/grid2_left.txt', unpack=True)
+
+offset = 5.0
+xs2_right -= offset
+xs2_left -= offset
+
+lambdas1 = np.array([lam(x, n=n+1, s=s, d=d) for n, x in enumerate(xs1)])
+f = open('build/lambdas_grid_cm.tex', 'w', 'utf-8')
+f.write(table([r'$x/\si{\milli\meter}$', r'$\lambda/\si{\nano\meter}$'],
+              [xs1, lambdas1*1e6]))
+f.close()
+
+d = ufloat(132, 5)
+lambdas2_right = np.array([lam(x, n=n+1, s=s, d=d)
+                           for n, x in enumerate(np.abs(xs2_right))])
+f = open('build/lambdas_grid_mm_r.tex', 'w', 'utf-8')
+f.write(table([r'$x/\si{\milli\meter}$', r'$\lambda/\si{\nano\meter}$'],
+              [xs2_right, lambdas2_right*1e6]))
+f.close()
+
+lambdas2_left = np.array([lam(x, n=n+1, s=s, d=d)
+                          for n, x in enumerate(np.abs(xs2_left))])
+f = open('build/lambdas_grid_mm_l.tex', 'w', 'utf-8')
+f.write(table([r'$x/\si{\milli\meter}$', r'$\lambda/\si{\nano\meter}$'],
+              [xs2_left, lambdas2_left*1e6]))
+f.close()
+
+lambdas = np.append(lambdas1, lambdas2_right)
+lambdas = np.append(lambdas, lambdas2_left)
+
+(open('build/lambda_grid.tex', 'w', 'utf-8')
+ .write(r'$\overline{{\lambda}}_\text{{Gitter}}'
+        r'= \SI{{{:.0f}+-{:.0f}}}{{\nano\meter}}$'
+        .format(unp.nominal_values(lambdas).mean() * 1e6,
+                unp.std_devs(lambdas).mean() * 1e6)))
