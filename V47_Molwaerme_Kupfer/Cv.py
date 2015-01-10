246db6d8db81ffd7cb5680048c1631d800a36b73
loligsmile <ms_r@hotmail.de>
2015-01-10 14:31:15 +0100

    sooo :D sry :S

--- V47_Molwaerme_Kupfer/Cv.py
+++ V47_Molwaerme_Kupfer/Cv.py
@@ -1,16 +1,22 @@
 #! /usr/bin/env python3
-# coding: utf-8
 
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+from codecs import open
+from textable import table
 from uncertainties import unumpy as unp
+from scipy.integrate import quad
+
 #Konstanten
 Masse = 342
 Molmasse = 63.546
+Dichte = 8.92*10**6
 Molaresvolumen = 7.11*10**-6
 Kompressionsmodul = 140*10**9
+
+
 # Einlesen der Daten
 R0_r, DR0_r, t_r, Dt_r, U_r, DU_r, I_r, DI_r, R1_r, DR1_r = np.genfromtxt('data/daten.txt', unpack=True)
 Tt0, Rt, alpha = np.genfromtxt('data/temp.txt', unpack=True)
@@ -21,6 +27,7 @@ Talpha = Tt0 + 270
 alpha = alpha*10**-6
 I_r = I_r * 10**-3
 DI_r = DI_r * 10**-3
+
 #Funktionen
 def func_alpha(x,a,b,c,d,e,f):
 	return a*x**5+b*x**4+c*x**3+d*x**2+e*x+f
@@ -54,11 +61,62 @@ popt_alpha_plot, pcov_alpha_plot = curve_fit(func_alpha, Talpha, alpha_plot)
 popt_alpha, pcov_alpha = curve_fit(func_alpha, Talpha, alpha)
 
 
-#Rechnung
+#Rechnung b)
 T_Cv = Temperatur(R0,R1,popt_temp1[0],popt_temp1[1],popt_temp1[2])
 
 Cv = Cp(popt_temp[0],popt_temp[1],popt_temp[2],R0,R1,I,t,U) - Cp_zu_Cv(T_Cv,popt_alpha[0],popt_alpha[1],popt_alpha[2],popt_alpha[3],popt_alpha[4],popt_alpha[5])
-print(Cv)
+
+f = open('build/Cv.tex', 'w', 'utf-8')
+f.write(table([r'$T/\si{\kelvin}$', r'$C_V/\si{\joule\per\mol\per\kelvin}$',
+               r'$T/\si{\kelvin}$', r'$C_V/\si{\joule\per\mol\per\kelvin}$',],
+              [T_Cv[:10],Cv[:10],
+               T_Cv[11:21], Cv[11:21]]))
+f.close()
+
+rules = np.linspace(50,350,1000)
+result1 = []
+result2 = []
+Theta_Debye = 345
+for i in range(len(rules)):
+	result1.append(quad(lambda x: x**4*np.exp(x)/(1-np.exp(x))**2,0,Theta_Debye/rules[i]))
+	result2.append(result1[i][0])
+	result2[i] *= 9*8.31439 * rules[i]**3 / Theta_Debye**3
+
+
+
+#Rechnung c)
+Cv_c = unp.uarray([0 for i in range(11)],[0 for i in range(11)])
+for i in range(11):
+	Cv_c[i] = Cv[i]
+
+Theta_t = unp.uarray([4.1, 3.2, 2.8, 2.4, 2.2, 2.0, 2.0, 2.0, 2.1, 2.4, 2.3],[0.05 for i in range(11)])
+Theta_arr = unp.uarray([0 for i in range(11)],[0 for i in range(11)])
+summe1 = 0
+summe2 = 0
+for i in range(len(Theta_t)):
+	Theta_arr[i] = Theta_t[i] * T_Cv[i]
+	summe1 += Theta_arr[i]
+	summe2 += Theta_t[i]
+Theta1 = summe1/len(Theta_arr)
+Theta2 = summe2/len(Theta_t)
+
+f = open('build/Theta.tex', 'w', 'utf-8')
+f.write(table([r'$T/\si{\kelvin}$', r'$C_V/\si{\joule\per\mol\per\kelvin}$',
+               r'$\frac{\Theta}{T}/\si{}$', r'$\Theta/\si{\kelvin}$',],
+              [T_Cv[:11],Cv[:11],
+               Theta_t[:11], Theta_arr[:11]]))
+f.close()
+
+#Rechnung d)
+V = Masse/Dichte
+vl = 4.7*10**3
+vs = 2.26*10**3
+h_quer = 1.054571726*10**-34
+k_B = 1.3806488*10**-23
+Avogadro = 6.02214129*10**23
+omega_D = ( 18 * np.pi**2 * Avogadro * Masse/ ( V * Molmasse * (1/vl**3+2/vs**3)))**(1/3)
+
+theta_D = h_quer*omega_D/k_B
 
 #Plots
 fig = plt.figure()
@@ -77,14 +135,17 @@ plt.plot(Talpha, alpha_plot,"+",label="Wertepaare")
 plt.plot(x,func_alpha(x,popt_alpha_plot[0],popt_alpha_plot[1],popt_alpha_plot[2],popt_alpha_plot[3],popt_alpha_plot[4],popt_alpha_plot[5]),"-",label="Regressionsfunktion")
 plt.xlabel(r"T/K")
 plt.ylabel(r"$\alpha \cdot 10^{-6}$ K")
+plt.legend(loc=2)
 plt.savefig("build/Cv_alpha.pdf")
 plt.close(fig)
 
 fig = plt.figure()
 x = np.linspace(65,310)
 plt.plot(unp.nominal_values(T_Cv), unp.nominal_values(Cv),"+",label="Cv")
+plt.plot(rules,result2,"-",label="Theorie")
 plt.xlabel(r"T/K")
 plt.ylabel(r"$C_V \cdot K \mathrm{mol} / J$  ")
+plt.legend(loc=2)
 plt.errorbar(unp.nominal_values(T_Cv), unp.nominal_values(Cv), yerr=unp.std_devs(Cv), xerr=unp.std_devs(T_Cv), fmt='b+', label='Messwerte')
 plt.savefig("build/Cv_Cv.pdf")
 plt.close(fig)
