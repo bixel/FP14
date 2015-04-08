67ac6a58b54318dcb3ffa3fd96c7331dde0d403b
Kevin H <kevin@kehei.de>
2015-04-08 12:18:51 +0200

    fit

--- V59_Modulation-Demodulation/calculations.py
+++ V59_Modulation-Demodulation/calculations.py
@@ -2,6 +2,7 @@
 import numpy as np
 from uncertainties import ufloat
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit as cf
 
 
 # Teil c)
@@ -28,8 +29,24 @@ with open('build/m2.tex', 'w') as f:
 # Teil e)
 # Abhängigkeit phase-Spannung
 T, U = np.genfromtxt('am-demodulation-e.txt', unpack=True)
-plt.plot(T, U, 'r+')
+
+
+# fit
+def func(x, a, b, c, d):
+    """ Simple cosine function
+    """
+    return a * np.cos(b * x + c) + d
+
+
+popt, pocov = cf(func, T, U, p0=[150, 0.01, -1, 0])
+
+# plt.plot(T, U, 'r+', label='Messpunkte')
+# plt.errorbar(T, U, yerr=0.1, label='Messpunkte', fmt='r+')
+ts = np.linspace(np.amin(T), np.amax(T), 200)
+plt.plot(ts, func(ts, *popt), label='Fit')
 plt.xlabel(r'$T/\mathrm{ns}$')
+plt.xlim(-5, 95)
 plt.ylabel(r'$U/\mathrm{mV}$')
+plt.legend(loc='best')
 plt.savefig('build/demodulation-cosinus.pdf')
 plt.clf()
