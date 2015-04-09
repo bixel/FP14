0900e10a7e1cb7db44e48f0d7f1260a4c2201ec9
Kevin Heinicke <kevin@kehei.de>
2015-04-09 11:01:45 +0200

    calculations

--- V59_Modulation-Demodulation/calculations.py
+++ V59_Modulation-Demodulation/calculations.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import numpy as np
 from uncertainties import ufloat
+from uncertainties import unumpy as unp
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit as cf
 
@@ -25,6 +26,36 @@ m2 = (U1 + U2) / UT
 print(m2)
 with open('build/m2.tex', 'w') as f:
     f.write(r'\num{{{:L}}}'.format(m2))
+
+# Teil d)
+t1s = unp.uarray([960, 1940, 2940, 3970, 5030], 5)
+t2s = unp.uarray([1090, 2150, 3175, 4175, 5190], 5)
+delta_ts = (t2s - t1s)[:3]
+delta_ts *= 1e-9
+for i in range(len(delta_ts)):
+    delta_ts[i] /= i + 1
+delta_t = delta_ts.mean()
+
+omega_t = ufloat(981, 2) * 1e3
+t_t = 1 / omega_t  # periodendauer des Trägers in ns
+tmin = t_t - delta_t
+tmax = t_t + delta_t
+fmin = 1 / tmax
+fmax = 1 / tmin
+delta_f = (fmax - fmin) / 2
+m = delta_f / omega_t
+
+with open('build/t_t.tex', 'w') as f:
+    f.write(r'\SI{{{:L}}}{{\nano\second}}'.format(t_t * 1e9))
+
+with open('build/delta_t.tex', 'w') as f:
+    f.write(r'\SI{{{:L}}}{{\nano\second}}'.format(delta_t * 1e9))
+
+with open('build/delta_f.tex', 'w') as f:
+    f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(delta_f / 1e3))
+
+with open('build/m.tex', 'w') as f:
+    f.write(r'\num{{{:L}}}'.format(m))
 
 # Teil e)
 # Abhängigkeit phase-Spannung
