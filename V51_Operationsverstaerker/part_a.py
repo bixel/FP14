b483e5f51cb01b8193e752606d738c3a6b358d8e
loligsmile <ms_r@hotmail.de>
2015-06-27 17:34:07 +0200

    Teil a und c

--- V51_Operationsverstaerker/part_a.py
+++ V51_Operationsverstaerker/part_a.py
@@ -4,47 +4,131 @@ from uncertainties import ufloat
 from uncertainties import unumpy as unp
 from scipy.optimize import curve_fit
 
-# Import Data
-nu_1, V_1, dV_1 = np.genfromtxt("data/data_a_1.txt",unpack=True)
-nu_2, V_2, dV_2 = np.genfromtxt("data/data_a_2.txt",unpack=True)
-nu_3, V_3, dV_3 = np.genfromtxt("data/data_a_3.txt",unpack=True)
-nu_4, V_4, dV_4 = np.genfromtxt("data/data_a_4.txt",unpack=True)
+R_N_1 = 1e6
+R_N_2 = 120e3
+R_N_3 = 4.7e3
+R_N_4 = 1e6
 
-nu_1_log, V_1_log, dV_1_log = np.log(nu_1), np.log(V_1), np.log(dV_1)
-nu_2_log, V_2_log, dV_2_log = np.log(nu_2), np.log(V_2), np.log(dV_2)
-nu_3_log, V_3_log, dV_3_log = np.log(nu_3), np.log(V_3), np.log(dV_3)
-nu_4_log, V_4_log, dV_4_log = np.log(nu_4), np.log(V_4), np.log(dV_4)
+R_1_1 = 100
+R_1_2 = 100
+R_1_3 = 100
+R_1_4 = 4.7e3
+
+R_in = 50
+U_ein = 15e-3
+
+R_1_1 += R_in
+R_1_2 += R_in
+R_1_3 += R_in
+R_1_4 += R_in
+
+# Import Data
+nu_1, U_1, dU_1 = np.genfromtxt("data/data_a_1.txt",unpack=True)
+nu_2, U_2, dU_2 = np.genfromtxt("data/data_a_2.txt",unpack=True)
+nu_3, U_3, dU_3 = np.genfromtxt("data/data_a_3.txt",unpack=True)
+nu_4, U_4, dU_4 = np.genfromtxt("data/data_a_4.txt",unpack=True)
+
+V_1 = unp.uarray(U_1,dU_1) / U_ein
+V_2 = unp.uarray(U_2,dU_2) / U_ein
+V_3 = unp.uarray(U_3,dU_3) / U_ein
+V_4 = unp.uarray(U_4,dU_4) / U_ein
+
+nu_1_log, V_1_log = np.log(nu_1)+0, unp.log(V_1)+0
+nu_2_log, V_2_log = np.log(nu_2)+0, unp.log(V_2)+0
+nu_3_log, V_3_log = np.log(nu_3)+0, unp.log(V_3)+0
+nu_4_log, V_4_log = np.log(nu_4)+0, unp.log(V_4)+0
 
 # Fit Data
 def func(x,a,b):
 	return a*x+b
 
-popt_1, pcov_1 = curve_fit(func,nu_1_log,V_1_log)
-popt_2, pcov_2 = curve_fit(func,nu_2_log[6:14],V_2_log[6:14])
-popt_3, pcov_3 = curve_fit(func,nu_3_log[12:18],V_3_log[12:18])
-popt_4, pcov_4 = curve_fit(func,nu_4_log[11:18],V_4_log[11:18])
+popt_1, pcov_1 = curve_fit(func,nu_1_log,unp.nominal_values(V_1_log))
+popt_2, pcov_2 = curve_fit(func,nu_2_log[6:14],unp.nominal_values(V_2_log[6:14]))
+popt_3, pcov_3 = curve_fit(func,nu_3_log[12:18],unp.nominal_values(V_3_log[12:18]))
+popt_4, pcov_4 = curve_fit(func,nu_4_log[11:18],unp.nominal_values(V_4_log[11:18]))
 
-# Calc Grenzfrequenz
+# Calcs
 
-V_grenz_1 = ufloat(V_1[0],dV_1[0]) / np.sqrt(2)
-V_grenz_2 = ufloat(V_2[0],dV_2[0]) / np.sqrt(2)
-V_grenz_3 = ufloat(V_3[0],dV_3[0]) / np.sqrt(2)
-V_grenz_4 = ufloat(V_4[0],dV_4[0]) / np.sqrt(2)
+def verstaerkung(v,r_1,r_N):
+	return ( r_1 / r_N - 1 / v )**-1
 
-V_grenz_1_log = unp.log(V_grenz_1)
-V_grenz_2_log = unp.log(V_grenz_2)
-V_grenz_3_log = unp.log(V_grenz_3)
-V_grenz_4_log = unp.log(V_grenz_4)
+V_grenz_1 = V_1[0] / np.sqrt(2)
+V_grenz_2 = V_2[0] / np.sqrt(2)
+V_grenz_3 = V_3[0] / np.sqrt(2)
+V_grenz_4 = V_4[0] / np.sqrt(2)
+
+V_grenz_1_log = unp.log(V_grenz_1) + 0
+V_grenz_2_log = unp.log(V_grenz_2) + 0
+V_grenz_3_log = unp.log(V_grenz_3) + 0
+V_grenz_4_log = unp.log(V_grenz_4) + 0
 
 nu_grenz_1_log = ( V_grenz_1_log - popt_1[1] ) / popt_1[0]
 nu_grenz_2_log = ( V_grenz_2_log - popt_2[1] ) / popt_2[0]
 nu_grenz_3_log = ( V_grenz_3_log - popt_3[1] ) / popt_3[0]
 nu_grenz_4_log = ( V_grenz_4_log - popt_4[1] ) / popt_4[0]
 
-nu_grenz_1 = unp.exp(nu_grenz_1_log)
-nu_grenz_2 = unp.exp(nu_grenz_2_log)
-nu_grenz_3 = unp.exp(nu_grenz_3_log)
-nu_grenz_4 = unp.exp(nu_grenz_4_log)
+nu_grenz_1 = unp.exp(nu_grenz_1_log) + 0
+nu_grenz_2 = unp.exp(nu_grenz_2_log) + 0 
+nu_grenz_3 = unp.exp(nu_grenz_3_log) + 0
+nu_grenz_4 = unp.exp(nu_grenz_4_log) + 0
+
+print("nu1 = " + str(nu_grenz_1))
+print("nu2 = " + str(nu_grenz_2))
+print("nu3 = " + str(nu_grenz_3))
+print("nu4 = " + str(nu_grenz_4))
+
+with open('build/a_nu_1.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\hertz}}'.format(nu_grenz_1))
+with open('build/a_nu_2.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_2 / 1e3 ))
+with open('build/a_nu_3.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_3 / 1e3 ))
+with open('build/a_nu_4.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_4 / 1e3 ))   
+
+print("nu1*V1 = " + str(nu_grenz_1 * V_1[0]))
+print("nu2*V2 = " + str(nu_grenz_2 * V_2[0]))
+print("nu3*V3 = " + str(nu_grenz_3 * V_3[0]))
+print("nu4*V4 = " + str(nu_grenz_4 * V_4[0]))
+
+with open('build/a_nu_1_V_1.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_1 * V_1[0] / 1e3 ))
+with open('build/a_nu_2_V_2.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_2 * V_2[0] / 1e3 ))
+with open('build/a_nu_3_V_3.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_3 * V_3[0] / 1e3 ))
+with open('build/a_nu_4_V_4.tex', 'w') as f:
+	f.write(r'\SI{{{:L}}}{{\kilo\hertz}}'.format(nu_grenz_4 * V_4[0] / 1e3 ))
+
+print('V_1 = ' + str( verstaerkung(V_1[0],R_1_1,R_N_1) ))
+print('V_2 = ' + str( verstaerkung(V_2[0],R_1_2,R_N_2) ))
+print('V_3 = ' + str( verstaerkung(V_3[0],R_1_3,R_N_3) ))
+print('V_4 = ' + str( verstaerkung(V_4[0],R_1_4,R_N_4) ))
+
+with open('build/a_V_1.tex', 'w') as f:
+	f.write(r'\num{{{:L}}}'.format( verstaerkung(V_1[0],R_1_1,R_N_1) ))
+with open('build/a_V_2.tex', 'w') as f:
+	f.write(r'\num{{{:L}}}'.format( verstaerkung(V_2[0],R_1_2,R_N_2) ))
+with open('build/a_V_3.tex', 'w') as f:
+	f.write(r'\num{{{:L}}}'.format( verstaerkung(V_3[0],R_1_3,R_N_3) ))
+with open('build/a_V_4.tex', 'w') as f:
+	f.write(r'\num{{{:L}}}'.format( verstaerkung(V_4[0],R_1_4,R_N_4) ))
+
+
+print('V_1 = ' + str(V_1[0]) + ' dR = ' + str(R_N_1 / R_1_1))
+print('V_2 = ' + str(V_2[0]) + ' dR = ' + str(R_N_2 / R_1_2))
+print('V_3 = ' + str(V_3[0]) + ' dR = ' + str(R_N_3 / R_1_3))
+print('V_4 = ' + str(V_4[0]) + ' dR = ' + str(R_N_4 / R_1_4))
+
+with open('build/a_dR_1.tex', 'w') as f:
+	f.write(r'\num{{{:0.4f}}}'.format( R_N_1 / R_1_1 ))
+with open('build/a_dR_2.tex', 'w') as f:
+	f.write(r'\num{{{:0.4f}}}'.format( R_N_2 / R_1_2 ))
+with open('build/a_dR_3.tex', 'w') as f:
+	f.write(r'\num{{{:0.4f}}}'.format( R_N_3 / R_1_3 ))
+with open('build/a_dR_4.tex', 'w') as f:
+	f.write(r'\num{{{:0.4f}}}'.format( R_N_4 / R_1_4 ))
+
 
 # Plot Data
 x_1 = np.linspace(nu_1_log[0]-1,nu_1_log[len(nu_1_log)-1]+1,10**5)
@@ -59,7 +143,7 @@ x_4_full = np.linspace(nu_4_log[0],nu_4_log[len(nu_4_log)-1],10**5)
 
 plt.title(r'Frequenzgang eines gekoppelten Verstärkers mit:'+ '\n' 
 	+ r'$R_N = 1\,\mathrm{M}\Omega;\, R_1 = 100\,\Omega$')
-plt.plot(nu_1_log, V_1_log,"xb",label=r'Messpunkte')
+plt.plot(nu_1_log, unp.nominal_values(V_1_log),"xb",label=r'Messpunkte')
 plt.plot(x_1,func(x_1,*popt_1),'-k', label=r'Ausgleichsgerade $f_1(x)$')
 plt.legend(loc='best')
 plt.grid()
@@ -70,7 +154,7 @@ plt.clf()
 
 plt.title(r'Frequenzgang eines gekoppelten Verstärkers mit:'+ '\n' 
 	+ r'$R_N = 120\,\mathrm{k}\Omega;\, R_1 = 100\,\Omega$')
-plt.plot(nu_2_log, V_2_log,"xb",label=r'Messpunkte')
+plt.plot(nu_2_log, unp.nominal_values(V_2_log),"xb",label=r'Messpunkte')
 plt.plot(x_2,func(x_2,*popt_2),'-k', label=r'Ausgleichsgerade $f_2(x)$')
 plt.axhline(y=unp.nominal_values(V_grenz_2_log),color='g',linestyle='--', 
 	label=r'$\log(\overline{V}/\sqrt{2})$')
@@ -85,7 +169,7 @@ plt.clf()
 
 plt.title(r'Frequenzgang eines gekoppelten Verstärkers mit:'+ '\n' 
 	+ r'$R_N = 4.7\,\mathrm{k}\Omega;\, R_1 = 100\,\Omega$')
-plt.plot(nu_3_log, V_3_log,"xb",label=r'Messpunkte')
+plt.plot(nu_3_log, unp.nominal_values(V_3_log),"xb",label=r'Messpunkte')
 plt.plot(x_3,func(x_3,*popt_3),'-k', label=r'Ausgleichsgerade $f_3(x)$')
 plt.axhline(y=unp.nominal_values(V_grenz_3_log),color='g',linestyle='--', 
 	label=r'$\log(\overline{V}/\sqrt{2})$')
@@ -100,7 +184,7 @@ plt.clf()
 
 plt.title(r'Frequenzgang eines gekoppelten Verstärkers mit:'+ '\n' 
 	+ r'$R_N = 1\,\mathrm{M}\Omega;\, R_1 = 4.7\,\mathrm{k}\Omega$')
-plt.plot(nu_4_log, V_4_log,"xb",label=r'Messpunkte')
+plt.plot(nu_4_log, unp.nominal_values(V_4_log),"xb",label=r'Messpunkte')
 plt.plot(x_4,func(x_4,*popt_4),'-k', label=r'Ausgleichsgerade $f_4(x)$')
 plt.axhline(y=unp.nominal_values(V_grenz_4_log),color='g',linestyle='--', 
 	label=r'$\log(\overline{V}/\sqrt{2})$')
@@ -110,6 +194,5 @@ plt.legend(loc='best')
 plt.grid()
 plt.xlabel(r'$\log(\nu/\mathrm{Hz})$')
 plt.ylabel(r'$\log(\overline{V})$')
-plt.ylim(-6,2)
 plt.savefig('build/plot_a_4.pdf')
 plt.clf()
---
+++ V51_Operationsverstaerker/part_c.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+from scipy.optimize import curve_fit
+from codecs import open
+from textable import table
+
+U_g = ufloat(1,0.1)
+R_v = 10e3
+R_N = 100e3
+
+I = U_g / R_v
+print(I)
+with open('build/c_I.tex', 'w') as f:
+	f.write(r'\Si{{{:L}}}{{\micro\ampere}}'.format(I / 1e6))
+
+nu, U_e_raw, dU_e, U_a_raw, dU_a = np.genfromtxt('data/data_c.txt', unpack=True)
+
+U_e = unp.uarray(U_e_raw,dU_e)
+U_a = unp.uarray(U_a_raw,dU_a)
+
+U_a_m = I * R_N
+
+print(U_a_m)
+with open('build/c_U_a.tex', 'w') as f:
+	f.write(r'\num{{{:L}}}'.format( U_a_m ))
+
+R_e = np.array(U_e / I)
+V = np.array(R_N / R_e)
+
+with open('build/c_table.tex', 'w', 'utf-8') as f:
+	f.write(table([r'$nu/\si{\hertz}$', r'$U_e/\si{\volt}$', r'$U_a/\si{\volt}$', 
+					r'$R_e/\si{\ohm}$', r'$V$'], 
+					[nu, U_e, U_a, R_e, V]))
+
+def func(x,a,b):
+	return a*x+b
+
+popt_1, pcov_1 = curve_fit(func,np.log(nu[:5]),np.log(unp.nominal_values(R_e[:5])))
+popt_2, pcov_2 = curve_fit(func,np.log(nu[:5]),np.log(unp.nominal_values(V[:5])))
+popt_3, pcov_3 = curve_fit(func,np.log(nu[:9]),np.log(unp.nominal_values(R_e[:9])))
+popt_4, pcov_4 = curve_fit(func,np.log(nu[:9]),np.log(unp.nominal_values(V[:9])))
+
+x1 = np.linspace(np.log(nu[0]),np.log(nu[4]),1e5)
+x2 = np.linspace(np.log(nu[0]),np.log(nu[8]),1e5)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(np.log(nu[:5]), np.log(unp.nominal_values(R_e[:5])),"xb",label=r'Messpunkte $R_e$')
+ax.plot(x1, func(x1,*popt_1),'k-')
+ax2 = ax.twinx()
+ax2.plot(np.log(nu[:5]), np.log(unp.nominal_values(V[:5])),"xr",label=r'Messpunkte $V$')
+ax2.plot(x1, func(x1,*popt_2),'k-')
+ax.legend(loc='center left')
+ax2.legend(loc='center right')
+ax.grid()
+ax.set_xlabel(r'$\log(\nu/\mathrm{Hz})$')
+ax.set_ylabel(r'$\log(R_e/\mathrm{\Omega})$')
+ax2.set_ylabel(r'$\log(V)$')
+plt.savefig('build/plot_c.pdf')
+plt.close(fig)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(np.log(nu), np.log(unp.nominal_values(R_e)),"xb",label=r'Messpunkte $R_e$')
+ax.plot(x2, func(x2,*popt_3),'k-')
+ax2 = ax.twinx()
+ax2.plot(np.log(nu), np.log(unp.nominal_values(V)),"xr",label=r'Messpunkte $V$')
+ax2.plot(x2, func(x2,*popt_4),'k-')
+ax.axvline(x=np.log(20e3),color='g',linestyle='--')
+ax.legend(loc='center left')
+ax2.legend(loc='center right')
+ax.grid()
+ax.set_xlabel(r'$\log(\nu/\mathrm{Hz})$')
+ax.set_ylabel(r'$\log(R_e/\mathrm{\Omega})$')
+ax2.set_ylabel(r'$\log(V)$')
+plt.savefig('build/plot_c_full.pdf')
+plt.close(fig)
